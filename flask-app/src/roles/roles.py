5100f3cc07aacc5b599a1441dac14df8d29be9c3
djweinmann <dylanweinman@gmail.com>
2024-04-16 18:53:50 -0400

    roles

--- flask-app/src/roles/roles.py
+++ flask-app/src/roles/roles.py
@@ -22,7 +22,7 @@ def get_roles():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Update sport information for a specific sport
+# Update information for a specific role
 @roles.route('/roles/<roleID>', methods=['PUT'])
 def update_specific_role(roleID):
     cursor = db.get_db().cursor()
@@ -60,3 +60,34 @@ def delete_sport(roleID):
     cursor = db.get_db().commit()
     return make_response(jsonify('Role deleted'), 200)
 
+# Get description of a certain role
+@roles.route('/roles/<roleID>', methods=['GET'])
+def get_roles(roleID):
+    cursor = db.get_db().cursor()
+    cursor.execute('''
+                   SELECT description
+                   FROM roles
+                   WHERE roleID = %s
+                   ''', roleID)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Update roleID
+@roles.route('/roles/<roleID>', methods=['PUT'])
+def update_specific_role(roleID):
+    cursor = db.get_db().cursor()
+    cursor.execute('''
+                   UPDATE roles
+                   SET roleID = %s
+                   WHERE roleID = %s;
+                   ''', (request.json['roleID'], roleID))
+    
+    cursor = db.get_db().commit()
+    return make_response(jsonify('Rule updated'), 200)
