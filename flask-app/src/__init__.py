5b12674da57bb9ba5ea9285f66a9c406b72f9cce
Arinjay Singh <arinjay.singh@gmail.com>
2024-04-16 19:45:25 -0400

    added new path

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -35,11 +35,13 @@ def create_app():
     # Import the various Beluprint Objects
     from src.customers.customers import customers
     from src.products.products  import products
+    from src.team_members.team_members import team_members
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.register_blueprint(customers,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(team_members,    url_prefix='/tm')
 
     # Don't forget to return the app object
     return app
